f00d2bd55ad434c72aabc053370ef3a504c1c4a2
ChenghanY <chenghan_yang@163.com>
2023-06-18 00:54:30 +0800

    第二版aop实现

--- src/test/java/com/james/initializr/annotation/WebMvcConfig.java
+++ src/test/java/com/james/initializr/annotation/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package com.james.initializr.annotation;
 
-import com.james.initializr.temp.RepeatedlyReadFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    /**
+/*    *//**
      * 把自己定义的方法拦截器注入进来
      * @return JsonSchemaInterceptor
-     */
+     *//*
     @Bean
     public HandlerInterceptor getSchemaInterceptor() {
         return new JsonSchemaInterceptor();
@@ -23,16 +22,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(getSchemaInterceptor());
-    }
-
-    @Bean
-    public FilterRegistrationBean repeatedlyReadFilter() {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        RepeatedlyReadFilter repeatedlyReadFilter = new RepeatedlyReadFilter();
-        registration.setFilter(repeatedlyReadFilter);
-        registration.addUrlPatterns("/*");
-        return registration;
-    }
+    }*/
 
 
 }
